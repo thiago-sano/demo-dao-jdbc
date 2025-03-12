12f178afbc7f51a16d3fd0663a4ae31b6a593fb2
Thiago Sano <thiago.sano@outlook.com>
2025-03-12 15:47:46 -0300

    feat: findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,20 +2,16 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
         // assim, o programa conhece apenas a interface SellerDao, não a implementação SellerDaoJDBC
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
